84f818b65a0768cfcb65abdd714824daa61eee08
Nasipa <nasipa.ibragimova@gmail.com>
2021-01-28 14:03:25 +0600

    added models and admin

--- todo/urls.py
+++ todo/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     path("test3/", third),
 ]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
